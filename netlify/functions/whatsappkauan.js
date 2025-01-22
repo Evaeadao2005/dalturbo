816226d0e4d5c59eff5e8cc790e6deca93cd2dfc
Evaeadao2005 <86280981+Evaeadao2005@users.noreply.github.com>
2025-01-22 15:26:02 -0300

    Update whatsappkauan.js

--- netlify/functions/whatsappkauan.js
+++ netlify/functions/whatsappkauan.js
@@ -10,7 +10,7 @@ exports.handler = async function(event, context) {
         saudacao = "Boa noite, vim por indicação do Influenciador @kauan_ariel_oficial, tenho interesse em adquirir um plano na DALTV";
     }
 
-    const whatsappUrl = `https://wa.me/558892063359?text=${encodeURIComponent(saudacao)}`;
+    const whatsappUrl = `https://wa.me/558893509111?text=${encodeURIComponent(saudacao)}`;
     
     return {
         statusCode: 301,  // Código de redirecionamento
