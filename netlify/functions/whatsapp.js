695c66aab189b8940bd772bf4874e2d18272282b
Evaeadao2005 <86280981+Evaeadao2005@users.noreply.github.com>
2025-04-17 04:38:46 -0300

    Update whatsapp.js

--- netlify/functions/whatsapp.js
+++ netlify/functions/whatsapp.js
@@ -5,13 +5,13 @@ exports.handler = async function(event, context) {
     let saudacao;
 
 if (adjustedHour >= 6 && adjustedHour < 12) {
-    saudacao = "Bom dia! Tenho interesse em um plano da DALTV e gostaria de fazer um teste.";
+    saudacao = "Bom dia! Tenho interesse em um plano da DALTV e gostaria de fazer um teste. (2)";
 } else if (adjustedHour >= 12 && adjustedHour < 18) {
-    saudacao = "Boa tarde! Tenho interesse em um plano da DALTV e queria saber se posso fazer um teste.";
+    saudacao = "Boa tarde! Tenho interesse em um plano da DALTV e queria saber se posso fazer um teste. (2)";
 } else if (adjustedHour >= 0 && adjustedHour < 6) {
-    saudacao = "Boa madrugada! Me interessei por um plano da DALTV e queria fazer um teste antes.";
+    saudacao = "Boa madrugada! Me interessei por um plano da DALTV e queria fazer um teste antes. (2)";
 } else {
-    saudacao = "Boa noite! Tenho interesse em um plano da DALTV e gostaria de testar antes de contratar.";
+    saudacao = "Boa noite! Tenho interesse em um plano da DALTV e gostaria de testar antes de contratar. (2)";
 }
 
     const numbers = ["558894492159", "558892063359", "558892532304"];
