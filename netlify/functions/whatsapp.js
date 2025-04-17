b355ca8ab6db92fc45e36699e681465896f0f252
Evaeadao2005 <86280981+Evaeadao2005@users.noreply.github.com>
2025-04-17 04:37:41 -0300

    Update whatsapp.js

--- netlify/functions/whatsapp.js
+++ netlify/functions/whatsapp.js
@@ -1,22 +1,25 @@
 exports.handler = async function(event, context) {
-    const hour = new Date().getHours();
+    const hour = new Date().getUTCHours() - 3;
+    const adjustedHour = (hour + 24) % 24;
+
     let saudacao;
 
-    if (hour >= 6 && hour < 12) {
+    if (adjustedHour >= 6 && adjustedHour < 12) {
         saudacao = "Bom dia, tenho interesse em adquirir um plano na DALTV";
-    } else if (hour >= 12 && hour < 18) {
+    } else if (adjustedHour >= 12 && adjustedHour < 18) {
         saudacao = "Boa tarde, tenho interesse em adquirir um plano na DALTV";
+    } else if (adjustedHour >= 0 && adjustedHour < 6) {
+        saudacao = "Boa madrugada, tenho interesse em adquirir um plano na DALTV";
     } else {
         saudacao = "Boa noite, tenho interesse em adquirir um plano na DALTV";
     }
 
-    const numbers = ["558894492159","558892063359","558892532304"];
+    const numbers = ["558894492159", "558892063359", "558892532304"];
     const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
-
     const whatsappUrl = `https://wa.me/${randomNumber}?text=${encodeURIComponent(saudacao)}`;
 
     return {
-        statusCode: 301,  // Código de redirecionamento
+        statusCode: 301,
         headers: {
             Location: whatsappUrl
         }
