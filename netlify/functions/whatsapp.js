618512515d557845305f71beddb038a83ae58801
Evaeadao2005 <86280981+Evaeadao2005@users.noreply.github.com>
2025-04-17 04:38:27 -0300

    Update whatsapp.js

--- netlify/functions/whatsapp.js
+++ netlify/functions/whatsapp.js
@@ -4,15 +4,15 @@ exports.handler = async function(event, context) {
 
     let saudacao;
 
-    if (adjustedHour >= 6 && adjustedHour < 12) {
-        saudacao = "Bom dia, tenho interesse em adquirir um plano na DALTV";
-    } else if (adjustedHour >= 12 && adjustedHour < 18) {
-        saudacao = "Boa tarde, tenho interesse em adquirir um plano na DALTV";
-    } else if (adjustedHour >= 0 && adjustedHour < 6) {
-        saudacao = "Boa madrugada, tenho interesse em adquirir um plano na DALTV";
-    } else {
-        saudacao = "Boa noite, tenho interesse em adquirir um plano na DALTV";
-    }
+if (adjustedHour >= 6 && adjustedHour < 12) {
+    saudacao = "Bom dia! Tenho interesse em um plano da DALTV e gostaria de fazer um teste.";
+} else if (adjustedHour >= 12 && adjustedHour < 18) {
+    saudacao = "Boa tarde! Tenho interesse em um plano da DALTV e queria saber se posso fazer um teste.";
+} else if (adjustedHour >= 0 && adjustedHour < 6) {
+    saudacao = "Boa madrugada! Me interessei por um plano da DALTV e queria fazer um teste antes.";
+} else {
+    saudacao = "Boa noite! Tenho interesse em um plano da DALTV e gostaria de testar antes de contratar.";
+}
 
     const numbers = ["558894492159", "558892063359", "558892532304"];
     const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
